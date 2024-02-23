00283859b0fdc3c70370aff7fa42a7994bc5e142
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2024-02-23 20:22:45 +0700

    Add user model

--- carDetection/user/models.py
+++ carDetection/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+occupations = ["Traffic police", "Local police", "Department police"]
+
+class User(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE) 
+    first_name = models.TextField()
+    last_name = models.TextField()
+    email = models.EmailField()
+    is_staff = models.BooleanField()
+    is_superuser = models.BooleanField()
+
+class Profile(User):
+    occupation = models.CharField(choices=occupations)
+    phone_number = models.TextField()
+    bio = models.TextField(default=None)
+
