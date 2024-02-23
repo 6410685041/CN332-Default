0fe58cc34a66b6da1797a27f3e3f8d143f6ea1cd
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2024-02-23 22:31:19 +0700

    edit model Profile

--- carDetection/user/models.py
+++ carDetection/user/models.py
@@ -1,17 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class User(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE) 
-    first_name = models.TextField()
-    last_name = models.TextField()
-    email = models.EmailField()
-    is_staff = models.BooleanField()
-    is_superuser = models.BooleanField()
-
-class Profile(User):
+class Profile(AbstractUser):
     occupations = {
         "TP": "Traffic police", 
         "LP": "Local police",
--- carDetection/user/views.py
+++ carDetection/user/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-
+from user.models import Profile
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
     return render("index.html")
+
+def edit_profile(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    profile = Profile.objects.get(id=request.user)
+    return render(request,'edit_profile.html',{'profile': profile})
+
+def submit_edit(request):
+    if request.method == 'POST':
+        profile = Profile.objects.get(id=request.user)
+
+        profile.first_name = request.POST['first_name']
+        profile.last_name = request.POST['last_name']
+        profile.email = request.POST['email']
+        profile.phone_number = request.POST['phone_number']
+        profile.bio = request.POST['bio']
+
+        profile.save()
+        return reverse("index.html")
