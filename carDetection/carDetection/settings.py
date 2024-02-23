31a30cb938f18a78deead91abd853c9be252e347
Supakrit Nithikethkul <6410615139@student.tu.ac.th>
2024-02-23 20:51:07 +0700

    BASEDIR template

--- carDetection/carDetection/settings.py
+++ carDetection/carDetection/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = "carDetection.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates/"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
