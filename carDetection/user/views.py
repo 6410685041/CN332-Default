4f1397c8f7b5454eb7a52643a99474c10de026a5
Pond <6410685033@student.tu.ac.th>
2024-02-24 14:49:13 +0700

    update authen view

--- carDetection/user/views.py
+++ carDetection/user/views.py
@@ -2,39 +2,51 @@ from django.shortcuts import render
 from user.models import Profile
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
 # Create your views here.
+
 
 def view_home(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
-    profile = Profile.objects.get(id=request.user)
-    data = { "profile" , profile }
-    return render("user/home.html", data)
+        return HttpResponseRedirect(reverse("login"))
+    profile = Profile.objects.get(id=request.user.id)
+    data = {
+        "profile": profile,
+    }
+
+    return render(request, "index.html", data)
+
 
 def view_profile(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
+        return HttpResponseRedirect(reverse("login"))
     profile = Profile.objects.get(id=request.user)
-    data = { "profile" , profile }
+    data = {
+        "profile": profile,
+    }
     return render("user/profile.html", data)
+
 
 def view_edit_profile(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
+        return HttpResponseRedirect(reverse("login"))
     profile = Profile.objects.get(id=request.user)
-    data = { "profile" , profile }
+    data = {
+        "profile": profile,
+    }
     return render("user/profile.html", data)
 
+
 def submit_edit_profile(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         profile = Profile.objects.get(id=request.user)
-        profile.first_name = request.POST['first_name']
-        profile.last_name = request.POST['last_name']
-        profile.email = request.POST['email']
-        profile.username = request.POST['username']
-        profile.occupation = request.POST['occupation']
-        profile.phone_number = request.POST['phone_number']
-        profile.bio = request.POST['bio']
+        profile.first_name = request.POST["first_name"]
+        profile.last_name = request.POST["last_name"]
+        profile.email = request.POST["email"]
+        profile.username = request.POST["username"]
+        profile.occupation = request.POST["occupation"]
+        profile.phone_number = request.POST["phone_number"]
+        profile.bio = request.POST["bio"]
         profile.save()
 
         return reverse("profile")
