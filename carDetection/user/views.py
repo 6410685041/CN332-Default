b0bff523f11531e02e68c09160e579993bdc0678
Supakrit Nithikethkul <6410615139@student.tu.ac.th>
2024-02-24 13:51:44 +0700

    Edit profile

--- carDetection/user/views.py
+++ carDetection/user/views.py
@@ -4,24 +4,30 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
 
-def index(request):
-    return render("index.html")
-
-def edit_profile(request):
+def view_profile(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
     profile = Profile.objects.get(id=request.user)
-    return render(request,'edit_profile.html',{'profile': profile})
+    data = { "profile" , profile }
+    return render("user/profile.html", data)
 
-def submit_edit(request):
+def view_edit_profile(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    profile = Profile.objects.get(id=request.user)
+    data = { "profile" , profile }
+    return render("user/profile.html", data)
+
+def submit_edit_profile(request):
     if request.method == 'POST':
         profile = Profile.objects.get(id=request.user)
-
         profile.first_name = request.POST['first_name']
         profile.last_name = request.POST['last_name']
         profile.email = request.POST['email']
+        profile.username = request.POST['username']
+        profile.occupation = request.POST['occupation']
         profile.phone_number = request.POST['phone_number']
         profile.bio = request.POST['bio']
-
         profile.save()
-        return reverse("index.html")
+        
+        return reverse("profile")
