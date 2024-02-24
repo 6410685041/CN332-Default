c44bd07d15128494fec30004cacc247bc7d5d203
Supakrit Nithikethkul <6410615139@student.tu.ac.th>
2024-02-24 13:30:31 +0700

    profile model

--- carDetection/user/models.py
+++ carDetection/user/models.py
@@ -8,6 +8,24 @@ class Profile(AbstractUser):
         ("DP", "Department of Highway"),
     )
     
-    occupation = models.CharField(max_length=2, choices=occupations)
-    phone_number = models.TextField()
-    bio = models.TextField(default=None, null=True, blank=True)
+    __occupation = models.CharField(max_length=2, choices=occupations)
+    __phone_number = models.TextField()
+    __bio = models.TextField(default=None, null=True, blank=True)
+
+    def get_occupation(self):
+        return self.__occupation
+
+    def get_phone_number(self):
+        return self.__phone_number
+    
+    def get_bio(self):
+        return self.__bio
+    
+    def set_occupation(self, value):
+        self.__occupation == value
+    
+    def set_phone_number(self, value):
+        self.__phone_number == value
+
+    def set_bio(self, value):
+        self.__bio == value
