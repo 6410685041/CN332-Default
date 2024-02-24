fbd58e49615215b14b109ebc7321b026b18acdc2
Pond <6410685033@student.tu.ac.th>
2024-02-24 16:20:00 +0700

    init my profile template

--- carDetection/user/views.py
+++ carDetection/user/views.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 
 def view_home(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("login"))
+        return HttpResponseRedirect(reverse("account_login"))
     profile = Profile.objects.get(id=request.user.id)
     data = {
         "profile": profile,
@@ -19,8 +19,8 @@ def view_home(request):
 
 def view_profile(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("login"))
-    profile = Profile.objects.get(id=request.user)
+        return HttpResponseRedirect(reverse("account_login"))
+    profile = Profile.objects.get(id=request.user.id)
     data = {
         "profile": profile,
     }
@@ -29,8 +29,8 @@ def view_profile(request):
 
 def view_edit_profile(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("login"))
-    profile = Profile.objects.get(id=request.user)
+        return HttpResponseRedirect(reverse("account_login"))
+    profile = Profile.objects.get(id=request.user.id)
     data = {
         "profile": profile,
     }
@@ -39,7 +39,7 @@ def view_edit_profile(request):
 
 def submit_edit_profile(request):
     if request.method == "POST":
-        profile = Profile.objects.get(id=request.user)
+        profile = Profile.objects.get(id=request.user.id)
         profile.first_name = request.POST["first_name"]
         profile.last_name = request.POST["last_name"]
         profile.email = request.POST["email"]
