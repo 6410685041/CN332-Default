7a33aec0b961935a1b48ee7dfa032db275d54b56
Pond <6410685033@student.tu.ac.th>
2024-02-24 22:22:01 +0700

    learning celery

---
+++ carDetection/process/tasks.py
@@ -0,0 +1,14 @@
+# myapp/tasks.py
+from celery import shared_task
+import time
+
+@shared_task
+def abc(x, y):
+    for i in range(1000000):
+        z = x*y*i
+    return z
+
+def xyz(x, y):
+    for i in range(1000000):
+        z = x*y*i
+    return z
