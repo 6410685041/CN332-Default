76ac3087418ebde8ef72f023c1e9021eb886853a
Pond <6410685033@student.tu.ac.th>
2024-02-24 16:07:16 +0700

    create base process template view

--- carDetection/process/views.py
+++ carDetection/process/views.py
@@ -3,45 +3,55 @@ from django.urls import reverse
 from process.models import Task, Intersection, Loop
 from django.http import HttpResponseRedirect
 from django.utils import timezone
+
 # Create your views here.
+
 
 # go to upload page (for upload video)
 def view_upload_task(request):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
-    return render(request, 'upload_task.html')
+        return HttpResponseRedirect(reverse("account_login"))
+    return render(request, "process/upload_task.html")
+
 
 def view_edit_task(request, task_id):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
+        return HttpResponseRedirect(reverse("account_login"))
     task = Task.objects.get(id=task_id)
-    data = {"task": task,}
-    return render(request, 'edit_task.html', data)
+    data = {
+        "task": task,
+    }
+    return render(request, "process/edit_task.html", data)
+
 
 def create_task(request):
-    if request.method == 'POST':
-        status = 'In process'
+    if request.method == "POST":
+        status = "In process"
         time = timezone.now()
-        video = request.POST['video']
-        intersection = request.POST['intersection']
+        video = request.POST["video"]
+        intersection = request.POST["intersection"]
         Intersection.objects.get_or_create(intersection_name=intersection)
-        task = Task.objects.create(video=video, intersection=intersection, time=time, status=status)
+        task = Task.objects.create(
+            video=video, intersection=intersection, time=time, status=status
+        )
         task.save()
-        reverse("edit_task" , task.id)
+        reverse("edit_task", task.id)
 
-# receive loop points 
+
+# receive loop points
 #       data - points
 #       task_id - for specific task
 def add_loop(request, data, task_id):
-    if request.method == 'POST':
-        
-        points=[] # encypt point from frontend
+    if request.method == "POST":
+
+        points = []  # encypt point from frontend
 
         task = Task.objects.get(id=task_id)
-        name = request.POST['loop_name']
+        name = request.POST["loop_name"]
         loop = Loop.objects.create(loop_name=name, points=points, task=task)
         loop.save()
         reverse("edit_task", task.id)
+
 
 def remove_loop(request, loop_id):
     loop = Loop.objects.get(id=loop_id)
@@ -49,8 +59,8 @@ def remove_loop(request, loop_id):
     loop.delete()
     reverse("edit_task", task_id)
 
+
 def remove_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
     reverse("my_queue")
-        
