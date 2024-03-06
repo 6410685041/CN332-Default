8a580b3d9365d03606a8389a1a3285e33020b06d
Supakrit Nithikethkul <6410615139@cn351>
2024-03-06 11:39:29 +0700

    del project

--- project/Process/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Car, Truck, Motorbike
-
-# Register your models here.
-
-admin.site.register(Car)
-admin.site.register(Truck)
-admin.site.register(Motorbike)
