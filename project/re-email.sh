290442f1c7ba386b26caa976561cf0a8481f6ad1
Supakrit Nithikethkul <6410615139@student.tu.ac.th>
2024-03-10 23:46:09 +0700

    the most danger file in this project

--- project/re-email.sh
+++ project/re-email.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # Check if two arguments are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 OLD_EMAIL NEW_EMAIL"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 OLD_EMAIL NEW_EMAIL NEW_NAME"
     exit 1
 fi
 
 OLD_EMAIL=$1
 NEW_EMAIL=$2
+NEW_NAME=$3
 
 # Apply the filter-branch command to change the commit author email
 git filter-branch -f --env-filter "
 if [ \"\$GIT_COMMITTER_EMAIL\" = \"$OLD_EMAIL\" ]; then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
     export GIT_COMMITTER_EMAIL=\"$NEW_EMAIL\"
 fi
 if [ \"\$GIT_AUTHOR_EMAIL\" = \"$OLD_EMAIL\" ]; then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
     export GIT_AUTHOR_EMAIL=\"$NEW_EMAIL\"
 fi
 " --tag-name-filter cat -- --branches --tags
