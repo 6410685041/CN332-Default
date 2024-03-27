81f061348e8da0e82821722504e1300e86e19de3
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2024-03-27 14:23:22 +0700

    Config version update a little bit

--- assignments/assignment5_bridgePattern/ConfigReader.py
+++ assignments/assignment5_bridgePattern/ConfigReader.py
@@ -1,21 +1,24 @@
 import json
 import configparser
 
-class ConfigReader:
-    def __init__(self, config_file):
-        self.config_file = config_file
+class ConfigAdapter:
+    def __init__(self, configAdaptee):
+        self.config_file = configAdaptee
         self.config_data = {}
         self.read_config()
 
     def read_config(self):
-        """To be implemented by subclasses."""
-        raise NotImplementedError
+        self.config_file.read_config()
 
     def get(self, key, default=None):
         """Retrieves a value from the configuration data."""
         return self.config_data.get(key, default)
+    
+    def get(self,section, key, default=None):
+        """Retrieves a value from the configuration data."""
+        return self.config_data.get(section,key, default)
 
-class JsonConfigReader(ConfigReader):
+class JsonConfigAdaptee(ConfigAdapter):
     def read_config(self):
         """Reads the JSON configuration file."""
         try:
@@ -26,7 +29,7 @@ class JsonConfigReader(ConfigReader):
         except json.JSONDecodeError:
             print(f"Error: The file {self.config_file} contains invalid JSON.")
 
-class IniConfigReader(ConfigReader):
+class IniConfigAdaptee(ConfigAdapter):
     def read_config(self):
         """Reads the INI configuration file."""
         self.config_data = configparser.ConfigParser()
@@ -38,18 +41,25 @@ class IniConfigReader(ConfigReader):
     def get(self, section, key, default=None):
         """Retrieves a value from a specific section in the INI configuration data."""
         try:
+            # return self.config_data.get(section, key)
             return self.config_data.get(section, key)
         except (configparser.NoSectionError, configparser.NoOptionError):
             return default
 
 """
 # Example usage
-json_reader = JsonConfigReader('config.json')
+json_reader = JsonConfigAdapter('config.json')
 print(json_reader.get('some_key'))
 
-ini_reader = IniConfigReader('config.ini')
+ini_reader = IniConfigAdapter('config.ini')
 print(ini_reader.get('SectionName', 'some_key'))
 """
 
-ini_reader = IniConfigReader('myhouse.ini')
-print(ini_reader.get('roof', 'color'))
+ini_reader = IniConfigAdaptee('myhouse.ini')
+# print(ini_reader.get(section='roof', key='color'))
+
+# json_reader = JsonConfigAdapter('myhouse.json')
+# print(json_reader.get(key='house')["roof"])
+
+ConfigAdapter = ConfigAdapter(ini_reader)
+print(ConfigAdapter.get(section="roof",key='color'))
