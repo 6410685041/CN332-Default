47c8209b5a3acf7a2aaa4940f0defe9a8fb0d502
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2024-03-30 20:10:32 +0700

    Add All type of shape

--- assignments/assignment5_bridgePattern/IniDraw_testSystem.py
+++ assignments/assignment5_bridgePattern/IniDraw_testSystem.py
@@ -1,12 +1,23 @@
 from ConfigAdapter import ConfigAdapter
 from ConfigAdaptee import IniConfigAdaptee
-from Shape.shape import Rectangle
+from Shape.shape import Rectangle, Triangle, Circle
 
 ini_reader = IniConfigAdaptee('myhouse.ini')
 adapter = ConfigAdapter(ini_reader)
 # print(adapter.get(query={'section':'house', 'key':'color'}),adapter.get(query={'section':'house', 'key':'coordinate'}))
 
-coordinate = adapter.get(query={'section':'house', 'key':'coordinate'})
-color = adapter.get(query={'section':'house', 'key':'color'})
-rectangle = Rectangle(coordinate=coordinate, color=color)
+coordinate_rec = adapter.get(query={'section':'house', 'key':'coordinate'})
+color_rec = adapter.get(query={'section':'house', 'key':'color'})
+rectangle = Rectangle(coordinate=coordinate_rec, color=color_rec)
 rectangle.draw()
+
+coordinate_tri = adapter.get(query={'section':'roof', 'key':'coordinate'})
+color_tri = adapter.get(query={'section':'roof', 'key':'color'})
+triangle = Triangle(coordinate=coordinate_tri, color=color_tri)
+triangle.draw()
+
+coordinate_cir = adapter.get(query={'section':'window', 'key':'coordinate'})
+color_cir = adapter.get(query={'section':'window', 'key':'color'})
+radius_cir = adapter.get(query={'section':'window', 'key':'radius'})
+circle = Circle(coordinate=coordinate_cir, color=color_cir, radius=radius_cir)
+circle.draw()
--- assignments/assignment5_bridgePattern/Shape/shape.py
+++ assignments/assignment5_bridgePattern/Shape/shape.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import turtle as t
-from .extension import parse_points
+from .extension import parse_points, calculate_angle
 
 class Shape(ABC):
     @abstractmethod
@@ -31,7 +31,7 @@ class Rectangle(Shape):
         t.fd(distance)
 
     def draw(self):
-        # t.color(self.color)
+        t.color(self.color)
         t.begin_fill()
         t.penup()
         t.goto(self.coordinates[0][0], self.coordinates[0][1])
@@ -42,3 +42,62 @@ class Rectangle(Shape):
 
         t.end_fill()
 
+class Triangle(Shape):
+    BOUND = 100
+
+    def __init__(self, coordinate, color):
+        coordinates = parse_points(coordinate)
+        coordinates = [(num1 * self.BOUND, num2 * self.BOUND) for (num1, num2) in coordinates]
+        coordinates.append(coordinates[0])
+
+        angle = []
+        angle.append(calculate_angle(coordinates[0],coordinates[1],coordinates[2]))
+        angle.append(calculate_angle(coordinates[1],coordinates[2],coordinates[0]))
+        angle.append(calculate_angle(coordinates[2],coordinates[0],coordinates[1]))
+
+        self.coordinates = coordinates
+        self.angle = angle
+        self.color = color
+
+    def drawline(self, distance):
+        t.fd(distance)
+
+    def draw(self):
+        t.color(self.color)
+        t.begin_fill() 
+
+        t.penup()
+        t.goto(self.coordinates[0][0], self.coordinates[0][1])
+        t.pendown()
+ 
+        for (x, y) in self.coordinates:
+           t.goto(x, y)
+
+        t.end_fill()
+
+class Circle(Shape):
+    BOUND=100
+
+    def __init__(self,radius,coordinate,color):
+        coordinates = parse_points(coordinate)
+        coordinates = [(num1 * self.BOUND, num2 * self.BOUND) for (num1, num2) in coordinates]
+
+        self.radius = float(radius)*self.BOUND
+        self.coordinates = coordinates
+        self.color = color
+
+    def drawline(self, distance):
+        t.fd(distance)
+
+    def draw(self):
+        t.color(self.color)
+        t.begin_fill() 
+
+        t.penup()
+        t.goto(self.coordinates[0][0], self.coordinates[0][1]-self.radius)
+        t.pendown()
+ 
+        t.circle(self.radius)
+
+        t.end_fill()
+        t.done()
