8a6a441832018423366c2154032f7d12068b67bc
Pond <6410685033@student.tu.ac.th>
2024-03-30 22:21:20 +0700

    trying config adapter pattern

--- assignments/assignment5_bridgePattern/Configuration.py
+++ assignments/assignment5_bridgePattern/Configuration.py
@@ -1,20 +1,6 @@
-from ConfigAdapter import ConfigAdapter
-from ConfigAdaptee import IniConfigAdaptee, JsonConfigAdaptee
-
 class Configuration:
-    def __init__(self, config_file):
-        try:
-            if config_file.endswith('.ini'):
-                self.config = ConfigAdapter(IniConfigAdaptee(config_file))
-            elif config_file.endswith('.json'):
-                self.config = ConfigAdapter(JsonConfigAdaptee(config_file))
-            else:
-                raise ValueError("Invalid config file format")
-        except Exception as e:
-            print(f"Error: {e}")
+    def __init__(self, config):
+        self.data = self.get()
     
     def get(self, query=None):
         return self.config.get()
-    
-    def draw(self):
-        pass
--- assignments/assignment5_bridgePattern/factory.py
+++ assignments/assignment5_bridgePattern/factory.py
@@ -1,10 +1,19 @@
-from Configuration import Configuration
+from ConfigAdapter import ConfigAdapter
+from ConfigAdaptee import IniConfigAdaptee, JsonConfigAdaptee
 
 class Factory:
     def __init__(self, config_file):
-        config = Configuration(config_file)
+        try:
+            if config_file.endswith('.ini'):
+                self.config = ConfigAdapter(IniConfigAdaptee(config_file))
+            elif config_file.endswith('.json'):
+                self.config = ConfigAdapter(JsonConfigAdaptee(config_file))
+            else:
+                raise ValueError("Invalid config file format")
+        except Exception as e:
+            print(f"Error: {e}")
 
-    def get(self):
+    def draw(self):
         pass
 
 f=Factory('myhouse.json')
