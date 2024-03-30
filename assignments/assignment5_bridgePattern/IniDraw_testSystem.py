99acdc254a0a2d924aa8a58b8b86945584b5d61f
Pond <6410685033@student.tu.ac.th>
2024-03-30 18:33:11 +0700

    fix Shape

--- assignments/assignment5_bridgePattern/IniDraw_testSystem.py
+++ assignments/assignment5_bridgePattern/IniDraw_testSystem.py
@@ -1,9 +1,9 @@
 from ConfigAdapter import ConfigAdapter
 from ConfigAdaptee import IniConfigAdaptee
-from Shape.Rectangle import Rectangle
+from Shape.shape import Rectangle
 
 ini_reader = IniConfigAdaptee('myhouse.ini')
 adapter = ConfigAdapter(ini_reader)
 # print(adapter.get(query={'section':'house', 'key':'color'}),adapter.get(query={'section':'house', 'key':'coordinate'}))
 
-rectangle = Rectangle()
+rectangle = Rectangle(adapter.get(query={'section':'house', 'key':'coordinate'}))
--- assignments/assignment5_bridgePattern/Shape/Shape.py
+++ assignments/assignment5_bridgePattern/Shape/Shape.py
@@ -1,9 +1,37 @@
 from abc import ABC, abstractmethod
+import turtle as t
+from .extension import parse_points
 
-class Shape:
+class Shape(ABC):
     @abstractmethod
-    def draw():
+    def draw(self):
         pass
 
-    def drawline():
+    @abstractmethod
+    def drawline(self, distance):
         pass
+class Rectangle(Shape):
+    BOUND = 100
+
+    def __init__(self, coordinate):
+        coordinates = parse_points(coordinate).sort()
+        coordinates = [(num1 * self.BOUND, num2 * self.BOUND) for (num1, num2) in coordinates]
+        coordinate_1 = (coordinates[1][0],coordinates[0][1])
+        coordinate_2 = (coordinates[0][0],coordinates[1][1])
+        coordinates.insert(1, coordinate_1)
+        coordinates.append(coordinate_2)
+        coordinates.append(coordinates[0])
+
+        # self.color = color
+
+    def drawline(self, distance):
+        t.fd(distance)
+
+    def draw(self):
+        # t.color(self.color)
+        t.begin_fill() 
+
+        for (x, y) in self.coordinates:
+           t.goto(x, y)
+
+        t.end_fill() 
