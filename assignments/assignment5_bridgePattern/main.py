6922588a4d6f2b7b6e9f5c0158c6b55b2120405f
Pond <6410685033@student.tu.ac.th>
2024-03-31 20:51:20 +0700

    add new config drawing file

--- assignments/assignment5_bridgePattern/main.py
+++ assignments/assignment5_bridgePattern/main.py
@@ -1,15 +1,11 @@
 from src.Factory import Factory
 
-config_path = "myhouse.ini"
-# config_path = "./myhouse.json"
+config_path = "config_drawing/cat.ini"
 
 f = Factory(config_path)
-
 d = f.getDrawing()
+
+
 # d.draw("my_house.png")
 # d.draw("my_house.jpg")
-d.draw("test.png")
-# coordinate_roof = json_config.get(query={'key':'house'})['roof']['coordinate']
-# color_roof = json_config.get(query={'key':'house'})['roof']['color']
-# roof = Triangle(coordinate=coordinate_roof, color=color_roof)
-# roof.draw()
+d.draw()
