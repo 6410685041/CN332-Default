3579b971a38fac3c11a783f0237c18d52496810b
Pond <6410685033@student.tu.ac.th>
2024-05-04 21:06:07 +0700

    first try of Clock.py

--- assignments/assignment7_statePattern/workspace/Clock.py
+++ assignments/assignment7_statePattern/workspace/Clock.py
@@ -1,3 +1,6 @@
+import State
+import Button
+
 # Interface for the Subscriber
 class Subscriber:
     def update(self, context):
@@ -5,29 +8,32 @@ class Subscriber:
 
 # Clock class
 class Clock(Subscriber):
-    def __init__(self, initial_state):
+    def __init__(self, initial_state: State):
         self.state = initial_state
         self.observers = []
         self.time = 0
 
-    def update(self):
-        pass
+    def update(self, press_type: str):
+        if press_type == "s":
+            self.state.short_press()
+        elif press_type == "l":
+            self.state.long_press()
 
-    def attach(self, observer):
+    def attach(self, observer: Button):
         self.observers.append(observer)
 
-    def detach(self, observer):
+    def detach(self, observer: Button):
         self.observers.remove(observer)
 
-    def notify(self):
-        for observer in self.observers:
-            observer.update(self)
+    # def notify(self):
+    #     for observer in self.observers:
+    #         observer.update(self)
 
-    def change_state(self, state):
-        self.state = state
+    # def change_state(self, state: State):
+    #     self.state = state
 
-    def long_press(self):
-        self.state.long_press()
+    # def long_press(self):
+    #     self.state.long_press()
 
-    def short_press(self):
-        self.state.short_press()
+    # def short_press(self):
+    #     self.state.short_press()
